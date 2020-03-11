ee4109208ecefa80cdba9d2049e1af9cd0efc4ce
smokra <ivan.smokrovic@gmail.com>
2020-03-11 13:48:45 +0100

    Fixing responsivness

--- src/js/script.js
+++ src/js/script.js
@@ -66,72 +66,7 @@ $('.menu-toggle').click(function () {
 // no more than 9 lines of code (no jquerz)
 // hint: addEventListener
 
-/*-------------------- CARD ------------------------*/
-
-const height = (elem) => {
-
-  return elem.getBoundingClientRect().height
-
-}
-
-const distance = (elemA, elemB, prop) => {
-
-  const sizeA = elemA.getBoundingClientRect()[prop]
-  const sizeB = elemB.getBoundingClientRect()[prop]
-
-  return sizeB - sizeA
-
-}
-
-const factor = (elemA, elemB, prop) => {
-
-  const sizeA = elemA.getBoundingClientRect()[prop]
-  const sizeB = elemB.getBoundingClientRect()[prop]
-
-  return sizeB / sizeA
-
-}
-
-// first find all cards
-// then check if cards lenght exists
-// then applz function
-console.log(document.querySelectorAll('.card'))
-
-document.querySelectorAll('.card').forEach((elem) => {
-
-  const head = elem.querySelector('.card__head')
-  const image = elem.querySelector('.card__image')
-  const author = elem.querySelector('.card__author')
-  const body = elem.querySelector('.card__body')
-  const foot = elem.querySelector('.card__foot')
-
-  elem.onmouseenter = () => {
-
-    elem.classList.add('hover')
-
-    const imageScale = 1 + factor(head, body, 'height')
-    image.style.transform = `scale(${ imageScale })`
-
-    const bodyDistance = height(foot) * -1
-    body.style.transform = `translateY(${ bodyDistance }px)`
-
-    const authorDistance = distance(head, author, 'height')
-    author.style.transform = `translateY(${ authorDistance }px)`
-
-  }
-
-  elem.onmouseleave = () => {
-
-    elem.classList.remove('hover')
-
-    image.style.transform = `none`
-    body.style.transform = `none`
-    author.style.transform = `none`
-
-  }
-})
-
-/* Contact form */
+/* -------------------------- Contact form -------------------------------- */
 (function ($) {
 
   var input = $('.validate-input .input100');
@@ -182,6 +117,70 @@ document.querySelectorAll('.card').forEach((elem) => {
       $(thisAlert).removeClass('alert-validate');
   }
 
-
-
 })(jQuery);
+
+/*-------------------- CARD ------------------------*/
+
+const height = (elem) => {
+
+  return elem.getBoundingClientRect().height
+
+}
+
+const distance = (elemA, elemB, prop) => {
+
+  const sizeA = elemA.getBoundingClientRect()[prop]
+  const sizeB = elemB.getBoundingClientRect()[prop]
+
+  return sizeB - sizeA
+
+}
+
+const factor = (elemA, elemB, prop) => {
+
+  const sizeA = elemA.getBoundingClientRect()[prop]
+  const sizeB = elemB.getBoundingClientRect()[prop]
+
+  return sizeB / sizeA
+
+}
+
+// first find all cards
+// then check if cards lenght exists
+// then applz function
+//console.log(document.querySelectorAll('.card'))
+
+document.querySelectorAll('.card').forEach((elem) => {
+
+  const head = elem.querySelector('.card__head')
+  const image = elem.querySelector('.card__image')
+  const author = elem.querySelector('.card__author')
+  const body = elem.querySelector('.card__body')
+  const foot = elem.querySelector('.card__foot')
+
+  elem.onmouseenter = () => {
+
+    elem.classList.add('hover')
+
+    const imageScale = 1 + factor(head, body, 'height')
+    image.style.transform = `scale(${ imageScale })`
+
+    const bodyDistance = height(foot) * -1
+    body.style.transform = `translateY(${ bodyDistance }px)`
+
+    const authorDistance = distance(head, author, 'height')
+    author.style.transform = `translateY(${ authorDistance }px)`
+
+  }
+
+  elem.onmouseleave = () => {
+
+    elem.classList.remove('hover')
+
+    image.style.transform = `none`
+    body.style.transform = `none`
+    author.style.transform = `none`
+
+  }
+})
+
