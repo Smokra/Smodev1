e9612a00447cf924cc5a219c65076fe12a6a18ca
smodev <ivan.smokrovic@gmail.com>
2021-01-12 12:51:33 +0100

    Adding lootie and spline animations

--- gulpfile.js/webpack.js
+++ gulpfile.js/webpack.js
@@ -4,7 +4,10 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   entry: {
-    script: helpers.parse('helpers.source/config.js.src/script.js')
+    script: helpers.parse('helpers.source/config.js.src/script.js'),
+    assets: helpers.parse('helpers.source/config.js.src/assets.js'),
+    main: helpers.parse('helpers.source/config.js.src/main.js'),
+    scene: helpers.parse('helpers.source/config.js.src/scene.js')
   },
   output: {
     path: path.resolve(`${__dirname}/${helpers.parse('helpers.dist/config.js.dist/')}`),
